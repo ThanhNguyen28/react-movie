c4cdc1bef9ccc589691d53afcd6e5f01e0b6266e
ThanhNguyen28 <cuuviho16101987@gmail.com>
2022-06-12 09:27:25 +0700

    first commit

--- src/components/hooks/useScroll.js
+++ src/components/hooks/useScroll.js
@@ -7,14 +7,14 @@ export function useScroll() {
   const [scrollDimensions, setScrollDimensions] = useState({scrollX, scrollY});
   
   const handleScrollDimensions = () => {
-    const scrollX = window.scrollY || document.documentElement.scrollWidth;
+    const scrollX = window.scrollhandleScrollDimensionsY || document.documentElement.scrollWidth;
     const scrollY = window.scrollY || document.documentElement.scrollTop;
     setScrollDimensions({scrollX, scrollY});
   }
 
   useEffect(() => {
     handleScrollDimensions();
-    window.addEventListener('scroll', handleScrollDimensions);
+    window.addEventListener('scroll', );
     return () => {
       window.removeEventListener('scroll', handleScrollDimensions);
     }
